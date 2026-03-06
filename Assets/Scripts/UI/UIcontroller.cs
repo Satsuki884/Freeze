c3bb3cfd8c674c378ded60d59bed7ae017bcd38c
Satsuki884 <stasyaklim884@gmail.com>
2026-03-06 18:16:36 +0200

    added ui controller

---
+++ Assets/Scripts/UI/UIcontroller.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UIcontroller : MonoBehaviour
+{
+    [Header("Canvases")]
+    [SerializeField] private GameObject _mainMenuPanel;
+    [SerializeField] private GameObject _gameplayPanel;
+
+    [Header("Panels")]
+    [SerializeField] private GameObject _pausePanel;
+
+    [Header("Menu Buttons")]
+    [SerializeField] private Button _startButton;
+    [SerializeField] private Button _soundButton;
+    [SerializeField] private Button _infoButton;
+
+    [Header("Game Buttons")]
+    [SerializeField] private Button _pauseButton;
+    [SerializeField] private Button _resumeButton;
+    [SerializeField] private Button _restartButton;
+
+    [Header("UI Elements")]
+    [SerializeField] private TMP_Text _scoreTextTMP;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        SetAllPanelsInactive();
+
+        if (_mainMenuPanel != null)
+            _mainMenuPanel.SetActive(true);
+
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        InitializeButtons();
+    }
+
+    void InitializeButtons()
+    {
+        if (_startButton != null)
+        {
+            _startButton.onClick.RemoveAllListeners();
+            _startButton.onClick.AddListener(StartGame);
+        }
+
+        if (_soundButton != null)
+        {
+            _soundButton.onClick.RemoveAllListeners();
+            _soundButton.onClick.AddListener(() => Debug.Log("Sound button clicked!"));
+        }
+
+        if (_infoButton != null)
+        {
+            _infoButton.onClick.RemoveAllListeners();
+            _infoButton.onClick.AddListener(() => Debug.Log("Info button clicked!"));
+        }
+
+        if (_pauseButton != null)
+        {
+            _pauseButton.onClick.RemoveAllListeners();
+            _pauseButton.onClick.AddListener(TogglePause);
+        }
+
+        if (_resumeButton != null)
+        {
+            _resumeButton.onClick.RemoveAllListeners();
+            _resumeButton.onClick.AddListener(Resume);
+        }
+
+        if (_restartButton != null)
+        {
+            _restartButton.onClick.RemoveAllListeners();
+            _restartButton.onClick.AddListener(RestartGame);
+        }
+    }
+
+    void StartGame()
+    {
+        SetAllPanelsInactive();
+
+        if (_gameplayPanel != null)
+            _gameplayPanel.SetActive(true);
+
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void TogglePause()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 0f;
+            isPaused = true;
+
+            if (_pausePanel != null)
+                _pausePanel.SetActive(true);
+        }
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        if (_pausePanel != null)
+            _pausePanel.SetActive(false);
+    }
+
+    void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void SetAllPanelsInactive()
+    {
+        if (_mainMenuPanel != null) _mainMenuPanel.SetActive(false);
+        if (_gameplayPanel != null) _gameplayPanel.SetActive(false);
+        if (_pausePanel != null) _pausePanel.SetActive(false);
+    }
+}
