9b7e920c307c85dc5c089a9e119e9f8cc1250c8f
Satsuki884 <stasyaklim884@gmail.com>
2026-03-08 18:16:36 +0200

    fixed minigame

--- Assets/Scripts/UI/SliderController.cs
+++ Assets/Scripts/UI/SliderController.cs
@@ -14,11 +14,22 @@ public class SliderController : MonoBehaviour
     float minX;
     float maxX;
 
-    // void Start()
-    // {
-    //     minX = -sliderArea.rect.width / 2;
-    //     maxX = sliderArea.rect.width / 2;
-    // }
+    public void StartSlider()
+    {
+        float areaWidth = sliderArea.rect.width;
+        float sliderWidth = slider.rect.width;
+
+        // враховуємо ширину повзунка
+        minX = -areaWidth / 2 + sliderWidth / 2;
+        maxX = areaWidth / 2 - sliderWidth / 2;
+
+        slider.anchoredPosition = new Vector2(minX, slider.anchoredPosition.y);
+
+        RandomizeGreenZone();
+
+        direction = 1;
+        active = true;
+    }
 
     void Update()
     {
@@ -32,53 +43,46 @@ public class SliderController : MonoBehaviour
         }
     }
 
-    public void StartSlider()
-    {
-        float width = sliderArea.rect.width;
-
-        minX = -width / 2;
-        maxX = width / 2;
-
-        RandomizeGreenZone();
-
-        slider.anchoredPosition = new Vector2(minX, slider.anchoredPosition.y);
-
-        direction = 1;
-        active = true;
-    }
-
     void MoveSlider()
     {
-        slider.anchoredPosition += Vector2.right * speed * direction * Time.unscaledDeltaTime;
+        float newX = slider.anchoredPosition.x + speed * direction * Time.unscaledDeltaTime;
 
-        if (slider.anchoredPosition.x > maxX)
-        {
+        // жорстке обмеження
+        newX = Mathf.Clamp(newX, minX, maxX);
+
+        slider.anchoredPosition = new Vector2(newX, slider.anchoredPosition.y);
+
+        // зміна напрямку
+        if (newX >= maxX)
             direction = -1;
-        }
 
-        if (slider.anchoredPosition.x < minX)
-        {
+        if (newX <= minX)
             direction = 1;
-        }
     }
 
     void RandomizeGreenZone()
     {
-        float width = sliderArea.rect.width;
+        float areaWidth = sliderArea.rect.width;
+        float zoneWidth = greenZone.rect.width;
 
-        float randomX = Random.Range(-width / 2 + 100, width / 2 - 100);
+        float minZone = -areaWidth / 2 + zoneWidth / 2;
+        float maxZone = areaWidth / 2 - zoneWidth / 2;
+
+        float randomX = Random.Range(minZone, maxZone);
 
         greenZone.anchoredPosition = new Vector2(randomX, greenZone.anchoredPosition.y);
     }
 
     void CheckHit()
     {
-        float pos = slider.anchoredPosition.x;
+        float sliderX = slider.anchoredPosition.x;
 
-        float min = greenZone.anchoredPosition.x - greenZone.rect.width / 2;
-        float max = greenZone.anchoredPosition.x + greenZone.rect.width / 2;
+        float zoneMin = greenZone.anchoredPosition.x - greenZone.rect.width / 2;
+        float zoneMax = greenZone.anchoredPosition.x + greenZone.rect.width / 2;
 
-        if (pos > min && pos < max)
+        active = false;
+
+        if (sliderX >= zoneMin && sliderX <= zoneMax)
         {
             MiniGameController.Instance.Success();
         }
@@ -86,7 +90,5 @@ public class SliderController : MonoBehaviour
         {
             MiniGameController.Instance.Fail();
         }
-
-        active = false;
     }
 }
